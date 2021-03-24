aa957eb89b392f631f1361d8ef8b8d77ed25fb58
kwanghoon.kim <kk03187693@gmail.com>
2021-03-24 23:48:23 +0900

    add logger example

---
+++ mvc/src/main/java/mvc/mvc/basic/LogTestController.java
@@ -0,0 +1,22 @@
+package mvc.mvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.info("info log={}", name);
+
+        return "ok";
+    }
+
+}

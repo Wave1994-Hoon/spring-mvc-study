e95b8da1c264e07f759f93cda22cb667f5b1159a
kwanghoon.kim <kk03187693@gmail.com>
2021-03-25 00:49:08 +0900

    add mapping controller

---
+++ mvc/src/main/java/mvc/mvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,22 @@
+package mvc.mvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @GetMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+}

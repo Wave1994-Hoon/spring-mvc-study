c47e344dbaa11edf1377b6f3beca297b45be4f17
kwanghoon.kim <kk03187693@gmail.com>
2021-03-27 01:35:19 +0900

    add model attrubute

--- mvc/src/main/java/mvc/mvc/basic/request/RequestParamController.java
+++ mvc/src/main/java/mvc/mvc/basic/request/RequestParamController.java
@@ -1,8 +1,12 @@
 package mvc.mvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
+import mvc.mvc.basic.HelloData;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,5 +24,24 @@ public class RequestParamController {
         log.info("username = {}, age = {}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1Old(@RequestParam String username, @RequestParam int age) {
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        return "ok";
     }
 }

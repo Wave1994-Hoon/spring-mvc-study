6d19d3bf7e113823768e15595fc4a7118ce41191
kwanghoon.kim <kk03187693@gmail.com>
2021-04-05 00:30:46 +0900

    add servlet mvc pattern content

--- servlet/src/main/java/servlet/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ servlet/src/main/java/servlet/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -12,8 +12,14 @@ import java.io.IOException;
 public class MvcMemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*
+        * - /WEB-INF
+        * 이 경로안에 JSP가 있으면 외부에서 직접 JSP를 호출할 수 없다.
+        * */
         String viewPath = "/WEB-INF/views/new-form.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+
+        /* 서버 내부에서 다시 호출이 발생 -> 다른 서블릿이나 JSP로 이동 */
         dispatcher.forward(request, response);
     }
 }

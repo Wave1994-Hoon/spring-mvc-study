bd1cdef0a980b9c7d4b103cf9d08c35abff84146
Wave1994-Hoon <kk03187693@gmail.com>
2021-04-11 01:26:21 +0900

    test

--- servlet/src/main/java/servlet/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ servlet/src/main/java/servlet/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/V3/*")  // V3 하위에 들어오는 요청은 무조건 호
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/V3/*")  // V3 하위에 들어오는 요청은 무조건 호출 
 public class FrontControllerServletV3 extends HttpServlet {
 
     private Map<String, ControllerV3> controllerMap = new HashMap<>();
